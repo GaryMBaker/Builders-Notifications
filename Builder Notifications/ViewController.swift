f4edd5122580ef3f96f904bd63cf2d71293a6083
Gary Baker <gary.baker@idealgap.co.nz>
2018-07-02 20:36:45 +1200

    This is the first commit to this repo

--- Builder Notifications/ViewController.swift
+++ Builder Notifications/ViewController.swift
@@ -9,9 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var likeButton: UIButton?
+    @IBOutlet weak var userName: UILabel?
+    @IBOutlet weak var passWord: UIInputView?
+
+    
+    
+    @IBAction func likedThis(sender: UIButton) {
+        userName?.text = "Blah"
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        
+       
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
